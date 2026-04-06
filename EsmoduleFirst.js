8f4180d852ee38ee65b1b95ee87fbe92e9318144
Ravi Raj <ravraj11447798@gmail.com>
2026-04-07 00:10:32 +0530

    Updated Node.js practice (fs, os, path, ES modules)

--- EsmoduleFirst.js
+++ EsmoduleFirst.js
@@ -1,8 +1,8 @@
-//import simpleEs ,{simpleEs2}from "./EsmoduleSec.mjs";
-//simpleEs(); 
-//simpleEs2();
-//import {simpleEs2 as simpleEs} from "./EsmoduleSec.mjs";
-//simpleEs();
+import simpleEs ,{simpleEs2}from "./EsmoduleSec.mjs";
+simpleEs(); 
+simpleEs2();
+import {simpleEs2 as simpleEs} from "./EsmoduleSec.mjs";
+simpleEs();
 import * as a2 from "./EsmoduleSec.mjs";
 console.log(a2);
 console.log(a2.simpleEs2());
