8d4bded422f051cb8c67e2d23cdb5b758bb62f6f
Qweerty114477 <ravraj11447798@gmail.com>
2026-04-06 19:35:46 +0530

    Add files via upload
    JS basics: working with core modules like fs and OS, reading files asynchronously, and understanding system memory.

---
+++ NodeJs/osmodule.js
@@ -0,0 +1,9 @@
+const os1= require('os');
+console.log(os1.freemem());
+console.log(os1.homedir());
+console.log(os1.totalmem());
+console.log(os1.hostname());
+console.log(os1.platform());
+console.log(os1.release());
+console.log(os1.uptime());
+console.log(os1.type());
